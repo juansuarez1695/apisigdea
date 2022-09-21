bc01da4dd5a8d1cb2086463f104d0b98ffde339f
juansuarez16 <juancho_1695@hotmail.com>
2022-09-21 12:11:06 -0500

    creacion de api consulta base de datos PGN

---
+++ conexion/conexion.js
@@ -0,0 +1,25 @@
+const sql = require('mssql');
+
+
+const dbSettings = {
+    user:'pazysalvospgn',
+    password:'PazSalvos2022',
+    server:"172.16.9.173",
+    database:"esignacap",
+    options: {
+        encrypt: true, 
+        trustServerCertificate: true 
+      }
+}
+
+
+exports.getConnection = async function() {  
+    try {       
+        const pool =  await sql.connect(dbSettings);        
+        return pool;
+    } catch (error) {        
+        console.log(`${error} error al conectar la Base de datos`);
+    }
+}
+
+
