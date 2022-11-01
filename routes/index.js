864b774a2644b35fe1955809045099ce8763e800
juansuarez1695 <107064771+juansuarez1695@users.noreply.github.com>
2022-11-01 16:58:01 -0500

    Modificacion control de errores

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,24 @@ const router = express.Router();
 const {getConnection} = require("../conexion/conexion");
 /* GET home page. */
 router.get('/', async function(req, res, next) {
-  const pool = await getConnection();
-  const result =  await pool.request().query(`SELECT esignapre.dbo.GET_NUMBER_OF_TASKS('${req.query.cedula}') as 'Numero actividades'`);
-  console.log(result.recordset[0]['Numero actividades']);
-  let response ;
-  if(result.recordset[0]['Numero actividades'] > 0){
-    response = `El usuario tiene ${result.recordset[0]['Numero actividades']} actividades pendientes` 
-  }else if(result.recordset[0]['Numero actividades'] == null){
-    response = `El usuario no existe`
-  }else{
-    response = `El usuario tiene ${result.recordset[0]['Numero actividades']} actividades pendientes` 
+  try {
+    const pool = await getConnection();
+    const result =  await pool.request().query(`SELECT esignapre.dbo.GET_NUMBER_OF_TASKS('${req.query.cedula}') as 'Numero actividades'`);
+    console.log(result.recordset[0]['Numero actividades']);
+    let response ;
+    if(result.recordset[0]['Numero actividades'] > 0){
+      response = `El usuario tiene ${result.recordset[0]['Numero actividades']} actividades pendientes` 
+    }else if(result.recordset[0]['Numero actividades'] == null){
+      response = `El usuario no existe`
+    }else{
+      response = `El usuario tiene ${result.recordset[0]['Numero actividades']} actividades pendientes` 
+    }
+    res.json({ title: 'Consumo SIGDEA',numactividades: response});
+  } catch (error) {
+    res.json({ error: 'error de Consumo SIGDEA',numactividades: 'no hay conexion en Base de datos esignacap'});
   }
-  res.json({ title: 'Consumo SIGDEA',numactividades: response});
+ 
 });
+
 
 module.exports = router;
